c63ebdedb1993d2b04c40d07e0bc1cfb5fdcaa38
mygosia <mygosia@gmail.com>
2017-04-06 19:18:55 +0200

    dodany koszt gwarancji

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function Telefon(marka, cena, kolor, ekran, waga) {
 	this.waga = waga;
 }
 Telefon.prototype.printInfo = function() {
-		console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ". Telefon ma przekątną ekranu równą " + this.ekran + ", a jego waga to " + this.waga + ".");
+		console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ". Telefon ma przekątną ekranu równą " + this.ekran + ", a jego waga to " + this.waga + ". Koszt przedłużonej gwarancji, to " + this.warrantyCost())
+		;}
+Telefon.prototype.warrantyCost = function() {
+	return this.cena * 0.22;
 }
 var iPhone6S = new Telefon("Apple", 2250, "srebrny", "4,5cala", "156g");
 var SamsungGalaxyS6 = new Telefon("Samsung", 1250, "czarny", "3cale", "250g");
